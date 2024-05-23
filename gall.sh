3007e9561a7ad0be4652d057bb38ea7a8dec9b6f
Holtzmann Patrick <107305562+PatrickHlz@users.noreply.github.com>
2024-05-23 23:38:53 +0200

    Update gall.sh

--- gall.sh
+++ gall.sh
@@ -1,19 +1,18 @@
 function gall {
-    git add .;
-    if [[ -z $1 ]] 
-        then 
-            commit_message=$(openssl rand -base64 12)
-            git commit -m $commit_message
-            git push
-            echo -----push avec un message aleatoire-----
+    git add .
+    if [[ -z $1 ]]; then 
+        commit_message=$(openssl rand -base64 12)
+        git commit -m $commit_message
+        git push
+        echo "-----push avec un message aléatoire-----"
+    else
+        git commit -m "$1"
+        git push
+        if [[ -z $2 ]]; then
+            echo "-----push sans submit-----"
         else
-        git commit -m $1;
-        git push;
-        if [[ -z $2 ]] 
-            then echo -----push sans submit-----
-            else
-                git tag -ma $2;
-                git push --follow-tags;
+            git tag -m "$2"
+            git push --follow-tags
         fi
     fi
-    }
+}

fb68885c9b6d59f2d460ad3888c09d9026505ecf
Holtzmann Patrick <107305562+PatrickHlz@users.noreply.github.com>
2024-06-08 13:34:02 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,6 +2,8 @@
 
 #change the methode you would like to use *1
 
+#if you put in the transfert.sh the config function for calling it for changing some parameters
+
 #example
 var test = 1 #change it to 2
 
